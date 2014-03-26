e74e5c2d22e8a8854f37212ac7266bb995799f0b
syeda <its_kubbie@hotmail.com>
2014-03-26 19:56:03 +0000

    first program

---
+++ program1.rb
@@ -0,0 +1,8 @@
+puts 'Do you like the Bulls?'
+answer = gets.chomp
+if answer == 'yes'
+  puts 'Go Bulls!!<3'
+else if answer == 'no'
+  puts 'Come back when you are a real basketball fan!!'
+ end 
+end
